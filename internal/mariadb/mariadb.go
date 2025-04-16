4d9adc1396e53fe4d93d4debc6efe6fc117c23e8
mohidpineda <mohid.ahmad@inslapineda.cat>
2025-04-16 20:01:15 +0200

    Implementació d'autentificació per fer ús del bot amb una base de dades comprovant l'ID de l'usuari.

--- internal/mariadb/mariadb.go
+++ internal/mariadb/mariadb.go
@@ -1,1 +1,49 @@
 package mariadb
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	_ "github.com/go-sql-driver/mysql"
+)
+
+var database *sql.DB
+
+func InitDB() (*sql.DB, error) {
+	godotenv.Load()
+	dbUser := os.Getenv("DB_USER")
+	dbPass := os.Getenv("DB_PASS")
+	dbName := os.Getenv("DB_NAME")
+	dbHost := os.Getenv("DB_HOST")
+	dbPort := os.Getenv("DB_PORT")
+
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", dbUser, dbPass, dbHost, dbPort, dbName)
+
+	var err error
+	database, err = sql.Open("mysql", dsn)
+	if err != nil {
+		return nil, fmt.Errorf("no s'ha pogut connectar a la base de dades: %w", err)
+	}
+
+	err = database.Ping()
+	if err != nil {
+		return nil, fmt.Errorf("no s'ha pogut fer ping a la base de dades: %w", err)
+	}
+	log.Println("Connexió a la base de dades establerta correctament")
+	return database, nil
+}
+
+func UserExists(db *sql.DB, userID int64) (bool, error) {
+	var id int64
+	err := db.QueryRow("SELECT id FROM usuaris WHERE id = ?", userID).Scan(&id)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return false, nil
+		}
+		return false, fmt.Errorf("error en comprovar l'usuari a la base de dades: %w", err)
+	}
+	return true, nil
+}
