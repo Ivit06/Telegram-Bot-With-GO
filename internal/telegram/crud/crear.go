df32428bdbc6748f4ebffc4488869d39d48ebb49
mohidpineda <mohid.ahmad@inslapineda.cat>
2025-05-05 16:49:14 +0200

    Creació de la funció per crear un usuari a la base de dades mestre.

--- internal/telegram/crud/crear.go
+++ internal/telegram/crud/crear.go
@@ -1,1 +1,22 @@
 package crud
+
+import (
+	"database/sql"
+	"log"
+)
+
+func CrearUsuari(db *sql.DB, id int64, rol, nombre, apellido, segundoApellido string) error {
+	stmt, err := db.Prepare("INSERT INTO usuaris (id, role, nom, pcognom, scognom) VALUES (?, ?, ?, ?, ?)")
+	if err != nil {
+		log.Printf("Error al preparar la consulta de creación de usuario: %v", err)
+		return err
+	}
+	defer stmt.Close()
+
+	_, err = stmt.Exec(id, rol, nombre, apellido, segundoApellido)
+	if err != nil {
+		log.Printf("Error al ejecutar la consulta de creación de usuario: %v", err)
+		return err
+	}
+	return nil
+}
