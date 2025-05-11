83dc09518703c2a8537116f6ed4616b43acc1076
mohidpineda <mohid.ahmad@inslapineda.cat>
2025-05-11 17:55:33 +0200

    Canvi de textos pels botons.

--- internal/telegram/keyboards/update_callback_query.go
+++ internal/telegram/keyboards/update_callback_query.go
@@ -9,12 +9,12 @@ import (
 func GetCRUDKeyboard() tgbotapi.InlineKeyboardMarkup {
 	return tgbotapi.NewInlineKeyboardMarkup(
 		tgbotapi.NewInlineKeyboardRow(
-			tgbotapi.NewInlineKeyboardButtonData("CREAR", "crud_crear"),
-			tgbotapi.NewInlineKeyboardButtonData("LLISTAR", "crud_llistar"),
+			tgbotapi.NewInlineKeyboardButtonData("Crear", "crud_crear"),
+			tgbotapi.NewInlineKeyboardButtonData("Llistar", "crud_llistar"),
 		),
 		tgbotapi.NewInlineKeyboardRow(
-			tgbotapi.NewInlineKeyboardButtonData("MODIFICAR", "crud_modificar"),
-			tgbotapi.NewInlineKeyboardButtonData("ELIMINAR", "crud_eliminar"),
+			tgbotapi.NewInlineKeyboardButtonData("Modificar", "crud_modificar"),
+			tgbotapi.NewInlineKeyboardButtonData("Eliminar", "crud_eliminar"),
 		),
 		tgbotapi.NewInlineKeyboardRow(
 			tgbotapi.NewInlineKeyboardButtonData("Tornar", "back"),
@@ -30,8 +30,8 @@ func GetNodeMetricsKeyboard(instance string) tgbotapi.InlineKeyboardMarkup {
 			tgbotapi.NewInlineKeyboardButtonData("RAM", fmt.Sprintf("get_ram_info_%s", instance)),
 		),
 		tgbotapi.NewInlineKeyboardRow(
-			tgbotapi.NewInlineKeyboardButtonData("STORAGE", fmt.Sprintf("get_storage_info_%s", instance)),
-			tgbotapi.NewInlineKeyboardButtonData("PORTS UP", fmt.Sprintf("get_active_ports_%s", instance)),
+			tgbotapi.NewInlineKeyboardButtonData("Storage", fmt.Sprintf("get_storage_info_%s", instance)),
+			tgbotapi.NewInlineKeyboardButtonData("Ports up", fmt.Sprintf("get_active_ports_%s", instance)),
 		),
 		tgbotapi.NewInlineKeyboardRow(
 			tgbotapi.NewInlineKeyboardButtonData("Tornar", "back_instance"),
